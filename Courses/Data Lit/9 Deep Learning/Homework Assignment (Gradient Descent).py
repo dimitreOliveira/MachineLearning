28cbaa6680779c455538f63a9b1aa0d79d8ab104
dimitreOliveira <dimitreandrew@gmail.com>
2019-03-30 20:25:43 -0300

    Update Data Lit Course - Week 5 & 9
    Updated Data Lit Course - Week 5 & 9 -Supervised Learning & Deep Learning material
    - Logistic_Regression_(Assignment).ipynb
    - Homework Assignment (Gradient Descent).py

--- Courses/Data Lit/9 Deep Learning/Homework Assignment (Gradient Descent).py
+++ Courses/Data Lit/9 Deep Learning/Homework Assignment (Gradient Descent).py
@@ -1,16 +1,19 @@
 import numpy as np
 
+
 def sigmoid(x):
-“””
-Calculate sigmoid
-“””
-return 1/(1+np.exp(-x))
+    """
+    Calculate sigmoid
+    """
+    return 1 / (1 + np.exp(-x))
+
 
 def sigmoid_prime(x):
-“””
-# Derivative of the sigmoid function
-“””
-return sigmoid(x) * (1 – sigmoid(x))
+    """
+    Derivative of the sigmoid function
+    """
+    return sigmoid(x) * (1 - sigmoid(x))
+
 
 learnrate = 0.5
 x = np.array([1, 2, 3, 4])
@@ -19,30 +22,30 @@ y = np.array(0.5)
 # Initial weights
 w = np.array([0.5, -0.5, 0.3, 0.1])
 
-### Calculate one gradient descent step for each weight
-### Note: Some steps have been consolidated, so there are
-### fewer variable names than in the above sample code
+# Calculate one gradient descent step for each weight
+# Note: Some steps have been consolidated, so there are
+# fewer variable names than in the above sample code
 
 # TODO: Calculate the node’s linear combination of inputs and weights
-h = None
+h = x * w
 
 # TODO: Calculate output of neural network
-nn_output = None
+nn_output = sigmoid(h)
 
 # TODO: Calculate error of neural network
-error = None
+error = np.sum(nn_output - y)
 
 # TODO: Calculate the error term
 # Remember, this requires the output gradient, which we haven’t
 # specifically added a variable for.
-error_term = None
+error_term = sigmoid_prime(error)
 
 # TODO: Calculate change in weights
-del_w = None
+del_w = error_term * learnrate
 
-print(‘Neural Network output:’)
+print('Neural Network output:')
 print(nn_output)
-print(‘Amount of Error:’)
+print('Amount of Error:')
 print(error)
-print(‘Change in Weights:’)
+print('Change in Weights:')
 print(del_w)
